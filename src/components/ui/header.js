a0eab457f16d35e58abb20eb72228896d2d6d8f6
Dante Cypress <dante@Dantes-MacBook-Pro.local>
2020-03-16 16:58:38 -0700

    added AppBar Logo

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/core/styles";
 
+import logo from "../../assets/logo.svg";
 function ElevationScroll(props) {
   const { children } = props;
   const trigger = useScrollTrigger({
@@ -19,7 +19,11 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: "3em"
+  },
+  logo: {
+    height: "7em"
   }
 }));
 
@@ -31,8 +35,8 @@ export default function Header(props) {
       <ElevationScroll>
         <AppBar position="fixed" color="secondary">
           {/*Toolbar- stack content horizontally, rather than vertically*/}
-          <Toolbar>
-            <Typography variant="h3">Cypress Development</Typography>
+          <Toolbar disableGutters>
+            <img src={logo} alt="logo" className={classes.logo} />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
