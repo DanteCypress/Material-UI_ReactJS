0485d521fcdbf8051626d5b88c6bf15b5763cfba
Dante Cypress <dante@Dantes-MacBook-Pro.local>
2020-03-16 15:23:42 -0700

    Added CreateMuiTheme

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,3 +1,33 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export default createMuiTheme({});
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#118E9D",
+      dark: "#0A535B",
+      light: "#14B2C9",
+      contrastText: "#FFFFFF",
+      gradient: {
+        lightMainGradient: "linear-gradient(#14B2C9, #118E9D)"
+      }
+    },
+    secondary: {
+      main: "#303F9F",
+      dark: "#1A237E",
+      light: "#3F51B5",
+      contrastText: "#FFFFFF"
+    },
+    extras: {
+      veryLightGreen: "#EBF5F7"
+    },
+    errorColor: "#FF0F54",
+    successColor: "#017C68"
+  },
+  typography: {
+    htmlFontSize: 10,
+    fontFamily: "Lato, Helvetica, Arial, sans-serif",
+    body2: {
+      letterSpacing: 1
+    }
+  }
+});
